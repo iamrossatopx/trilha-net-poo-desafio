a12c3a02bfa8829e68ca8d74b79cdb328aca2d07
I am [Px] <12368182+iamrossatopx@users.noreply.github.com>
2024-01-09 12:59:45 -0400

    resolução
    implementação de heranças  e construtores e sobrescrição do metodo instalar aplicativo

--- Program.cs
+++ Program.cs
@@ -1,3 +1,9 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Smartphone phone1 = new Iphone("9090","Iphone 3gs", "2345meia78", 64);
+Smartphone phone2 = new Nokia("9090","Nokia 3310", "666paunasuaoreia", 128);
+
+phone1.InstalarAplicativo("Whatsapp");
+phone2.InstalarAplicativo("Whatsapp");
